ada40077787b7135c1e39910b195b3f64e009981
Sydney Cavallaro <sydneycavallaro@MacBook-Pro-3.local>
2021-06-29 17:36:55 -0400

    creation of scraping documents as well as layout changes

---
+++ static/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrapedrandomcoaster
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/randomcoasterDB")
+
+@app.route("/")
+def home():
+    coasterz = mongo.db.coasterz.find_one()
+    return render_template("index.html", random_data=coasterz)
+
+@app.route("/scrape")
+def scrape():
+    random_data = scrapedrandomcoaster.scrape()
+    mongo.db.coasterz.update({}, random_data, upsert=True)
+    
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
