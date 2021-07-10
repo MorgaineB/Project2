c43d0a183b4ee79dae67afe9acd974f03bffe0ac
Morgaine B <morgainebowers@gmail.com>
2021-07-10 10:41:33 -0400

    Added all pop ups

--- static/js/records.js
+++ static/js/records.js
@@ -24,7 +24,7 @@ var drops_url = "https://how-we-roll.herokuapp.com/api/records/drop_geo"
 $.getJSON(drops_url, function (geojson) {
     var geojsonLayer = L.geoJson(geojson, {
       onEachFeature: function (feature, layer) {
-        layer.bindPopup('<h1>'+feature.properties['Roller Coaster']+'</h1><p>name: ');
+        layer.bindPopup('<h2>'+feature.properties['Roller Coaster']+'</h2><hr><h5>Location: '+feature.properties.City+', '+feature.properties.Country+'</h5><h5>Rank: '+feature.properties.Rank+'</h5><h5>Drop: '+feature.properties.Drop+'</h5>');
       },
       style: function (feature) {
       }
@@ -38,6 +38,9 @@ var heights_url = "https://how-we-roll.herokuapp.com/api/records/height_geo"
 
 $.getJSON(heights_url, function (geojson) {
     var geojsonLayer = L.geoJson(geojson, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup('<h2>'+feature.properties['Roller Coaster']+'</h2><hr><h5>Location: '+feature.properties.City+', '+feature.properties.Country+'</h5><h5>Rank: '+feature.properties.Rank+'</h5><h5>Height: '+feature.properties.Height+'</h5>');
+          },
         style: function (feature) {
         }
     }).addTo(myMap);
@@ -50,6 +53,9 @@ var length_url = "https://how-we-roll.herokuapp.com/api/records/length_geo"
 
 $.getJSON(length_url, function (geojson) {
     var geojsonLayer = L.geoJson(geojson, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup('<h2>'+feature.properties['Roller Coaster']+'</h2><hr><h5>Location: '+feature.properties.City+', '+feature.properties.Country+'</h5><h5>Rank: '+feature.properties.Rank+'</h5><h5>Length: '+feature.properties.Length+'</h5>');
+          },
         style: function (feature) {
         }
     }).addTo(myMap);
@@ -62,45 +68,11 @@ var speed_url = "https://how-we-roll.herokuapp.com/api/records/speed_geo"
 
 $.getJSON(speed_url, function (geojson) {
     var geojsonLayer = L.geoJson(geojson, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup('<h2>'+feature.properties['Roller Coaster']+'</h2><hr><h5>Location: '+feature.properties.City+', '+feature.properties.Country+'</h5><h5>Rank: '+feature.properties.Rank+'</h5><h5>Speed: '+feature.properties.Speed+'</h5>');
+          },
         style: function (feature) {
         }
     }).addTo(myMap);
     controlLayers.addOverlay(geojsonLayer, "Seped Record Holders");
 });
-
-
-//ATTEMPT THREE -HEROKU DEPLOYMENT
-
-// // // Creating map object
-// var myMap = L.map("map", {
-//     'center': [34.1413, -32.7308],
-//     'zoom': 3,
-//     'layers': [
-//         L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/streets-v11",
-//     accessToken: API_KEY
-//         })]
-// });
-
-// // //Control Layer
-// var controlLayers = L.control.layers().addTo(myMap)
-
-// var drops_url = "https://how-we-roll.herokuapp.com/api/records/drop_geo";
-
-// d3.json(drops_url).then(function(response) {
-
-//   console.log(response);
-
-//   for (var i = 0; i < response.length; i++) {
-//     var location = response[i].location;
-
-//     if (location) {
-//       L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
-//     }
-//   }
-
-// });
